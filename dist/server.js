4c00c3307b59c1941f7fc105bb4ac6580805e3a0
DouglasKyrius <douglaskyrius@gmail.com>
2020-11-21 12:42:44 -0400

    dist build

---
+++ dist/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
+require("reflect-metadata");
+var express_1 = __importDefault(require("express"));
+require("./database");
+var routes_1 = __importDefault(require("./routes"));
+var app = express_1.default();
+app.use(express_1.default.json());
+app.use(routes_1.default);
+dotenv_1.default.config();
+app.listen(process.env.PORT || 3333, function () {
+    console.log("\uD83C\uDFC3 Running Server on port " + process.env.PORT);
+});
